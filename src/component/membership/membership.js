48be25e94eae64024213e86e66bc957a2391ac0a
jeongwon119 <badaim1009@gmail.com>
2021-11-24 10:07:17 +0900

    membership

---
+++ src/component/membership/membership.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import './membership.css'
+
+class Membership extends Component {
+    render() {
+        return (
+            <>
+                <div class="wrap">
+                    <div class="title_container">
+                        <div>
+                            <h1 id="title">MEMBERSHIP</h1>
+                        </div>
+                    </div>
+                    <div class="contents_container">
+                        <div class="premium_container">
+                            <div class="premium_text_container">
+                                <div>
+                                    <h1 id="premium_text">PREMIUM</h1>
+                                </div>
+                            </div>
+                            <div class="premium_provide1">
+                                <div>
+                                    <p id="premium_provide1">Free exercise</p>
+                                </div>
+                            </div>
+                            <div class="premium_provide2">
+                                <div>
+                                    <p id="premium_provide2">Online Tutorial</p>
+                                </div>
+                            </div>
+                            <div class="premium_provide3">
+                                <div>
+                                    <p id="premium_provide3">Certification</p>
+                                </div>
+                            </div>
+                            <div class="premium_provide4">
+                                <div>
+                                    <p id="premium_provide4">10$ month</p>
+                                </div>
+                            </div>
+                            <div class="premium_provide5">
+                                <div>
+                                    <a id="premium_provide5" href="#">ENROLLE NOW</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="community_container">
+                            <div class="community_text_container">
+                                <div>
+                                    <h1 id="community_text">COMMUNITY</h1>
+                                </div>
+                            </div>
+                            <div class="community_provide1">
+                                <div>
+                                    <p id="community_provide1">Free exercise</p>
+                                </div>
+                            </div>
+                            <div class="community_provide2">
+                                <div>
+                                    <p id="community_provide2">10 Online Tutorial</p>
+                                </div>
+                            </div>
+                            <div class="community_provide5">
+                                <div>
+                                    <a id="community_provide5" href="#">SIGN IN</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="expert_container">
+                            <div class="expert_text_container">
+                                    <div>
+                                        <h1 id="expert_text">EXPERT</h1>
+                                    </div>
+                                </div>
+                                <div class="expert_provide1">
+                                    <div>
+                                        <p id="expert_provide1">Free exercise</p>
+                                    </div>
+                                </div>
+                                <div class="expert_provide2">
+                                    <div>
+                                        <p id="expert_provide2">Online Tutorial</p>
+                                    </div>
+                                </div>
+                                <div class="expert_provide3">
+                                    <div>
+                                        <p id="expert_provide3">Certification</p>
+                                    </div>
+                                </div>
+                                <div class="expert_provide4">
+                                    <div>
+                                        <p id="expert_provide4">Certification</p>
+                                    </div>
+                                </div>
+                                <div class="expert_provide5">
+                                    <div>
+                                        <p id="expert_provide5">15$ month</p>
+                                    </div>
+                                </div>
+                                <div class="expert_provide6">
+                                    <div>
+                                        <a id="expert_provide6" href="#">ENROLLE NOW</a>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Membership
